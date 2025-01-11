e1ef02b977cf6d0cecfe3f00bd2509997e254197
MarharytaOsypova <osipovamv03@gmail.com>
2025-01-11 20:48:41 +0200

    react2hm5

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -9,6 +9,8 @@ export default function MoviesPage() {
   const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get('query') || '';
+    const [loading, setLoading] = useState(false);  
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (query) {
@@ -17,6 +19,8 @@ export default function MoviesPage() {
   }, [query]);
 
   const fetchMovies = async searchQuery => {
+    setLoading(true);  
+    setError(null);
     const url = `${API_BASE_URL}/search/movie?query=${searchQuery}`;
     try {
       const response = await axios.get(url, {
@@ -26,11 +30,14 @@ export default function MoviesPage() {
       });
       setMovies(response.data.results);
     } catch (error) {
+       setError('Error fetching search results');
       console.error('Error fetching search results:', error);
+    }finally {
+      setLoading(false);  
     }
   };
 
-  const handleSearch = async event => {
+  const handleSearch =   event => {
     event.preventDefault();
     const searchQuery = event.target.elements.query.value.trim();
 
@@ -39,15 +46,17 @@ export default function MoviesPage() {
     }
 
     setSearchParams({ query: searchQuery });
-    fetchMovies(searchQuery);
+     
   };
 
   return (
     <div>
       <form className={styles.form} onSubmit={handleSearch}>
         <input className={styles.input} type="text" name="query" defaultValue={query} />
-        <button type="submit">Search</button>
+        <button type="submit" disabled={loading}>Search</button>
       </form>
+       {loading && <p>Loading...</p>}  
+      {error && <p>{error}</p>}
       <MovieList movies={movies} />
     </div>
   );
